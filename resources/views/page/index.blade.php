fb64c9325ea5dec9fdf3b04e90c782f868c1b0ad
Mladen Petrov <bksi@abv.bg>
2017-12-19 03:56:45 +0200

    SOme changes

---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: mladen
+ * Date: 12/18/2017
+ * Time: 1:56 AM
+ */
---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: mladen
+ * Date: 12/18/2017
+ * Time: 1:57 AM
+ */
